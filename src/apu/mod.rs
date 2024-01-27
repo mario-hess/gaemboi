40bd2e39e2b4e2ccf551c3f9050f1c9a70b4650a
mario-hess <office.hess@gmail.com>
2024-01-27 18:39:59 +0100

    :sparkles: Add mixer

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,8 +1,10 @@
 mod channel;
+mod mixer;
 
 use crate::apu::channel::noise_channel::NoiseChannel;
 use crate::apu::channel::square_channel::{ChannelType, SquareChannel};
 use crate::apu::channel::wave_channel::{WaveChannel, WAVE_PATTERN_END, WAVE_PATTERN_START};
+use crate::apu::mixer::Mixer;
 
 pub const CH1_START: u16 = 0xFF10;
 pub const CH1_END: u16 = 0xFF14;
@@ -25,17 +27,10 @@ pub const AUDIO_END: u16 = WAVE_PATTERN_END;
 
 pub struct Apu {
     ch1: SquareChannel,
-    ch1_right: bool,
-    ch1_left: bool,
     ch2: SquareChannel,
-    ch2_right: bool,
-    ch2_left: bool,
     ch3: WaveChannel,
-    ch3_right: bool,
-    ch3_left: bool,
     ch4: NoiseChannel,
-    ch4_right: bool,
-    ch4_left: bool,
+    mixer: Mixer,
     right_volume: u8,
     left_volume: u8,
     enabled: bool,
@@ -46,17 +41,10 @@ impl Apu {
     pub fn new() -> Self {
         Self {
             ch1: SquareChannel::new(ChannelType::CH1),
-            ch1_right: false,
-            ch1_left: false,
             ch2: SquareChannel::new(ChannelType::CH2),
-            ch2_right: false,
-            ch2_left: false,
             ch3: WaveChannel::new(),
-            ch3_right: false,
-            ch3_left: false,
             ch4: NoiseChannel::new(),
-            ch4_right: false,
-            ch4_left: false,
+            mixer: Mixer::default(),
             right_volume: 0,
             left_volume: 0,
             enabled: false,
@@ -76,7 +64,7 @@ impl Apu {
             CH3_START..=CH3_END => self.ch3.read_byte(address),
             CH4_START..=CH4_END => self.ch4.read_byte(address),
             MASTER_VOLUME => self.get_master_volume(),
-            PANNING => self.get_panning(),
+            PANNING => u8::from(self.mixer),
             MASTER_CONTROL => self.get_master_control(),
             WAVE_PATTERN_START..=WAVE_PATTERN_END => self.ch3.read_wave_ram(address),
             _ => {
@@ -99,7 +87,7 @@ impl Apu {
             CH3_START..=CH3_END => self.ch3.write_byte(address, value),
             CH4_START..=CH4_END => self.ch4.write_byte(address, value),
             MASTER_VOLUME => self.set_master_volume(value),
-            PANNING => self.set_panning(value),
+            PANNING => self.mixer.set_panning(value),
             MASTER_CONTROL => self.set_master_control(value),
             WAVE_PATTERN_START..=WAVE_PATTERN_END => self.ch3.write_wave_ram(address, value),
             _ => eprintln!(
@@ -137,29 +125,5 @@ impl Apu {
     fn set_master_volume(&mut self, value: u8) {
         self.right_volume = (value & 0x07) + 1;
         self.left_volume = ((value & 0x70) >> 4) + 1;
-    }
-
-    fn get_panning(&self) -> u8 {
-        let ch1_right = if self.ch1_right { 0x01 } else { 0x00 };
-        let ch2_right = if self.ch2_right { 0x02 } else { 0x00 };
-        let ch3_right = if self.ch3_right { 0x04 } else { 0x00 };
-        let ch4_right = if self.ch4_right { 0x08 } else { 0x00 };
-        let ch1_left = if self.ch1_left { 0x10 } else { 0x00 };
-        let ch2_left = if self.ch2_left { 0x20 } else { 0x00 };
-        let ch3_left = if self.ch3_left { 0x40 } else { 0x00 };
-        let ch4_left = if self.ch4_left { 0x80 } else { 0x00 };
-
-        ch1_right | ch2_right | ch3_right | ch4_right | ch1_left | ch2_left | ch3_left | ch4_left
-    }
-
-    fn set_panning(&mut self, value: u8) {
-        self.ch1_right = value & 0x01 != 0;
-        self.ch2_right = value & 0x02 != 0;
-        self.ch3_right = value & 0x04 != 0;
-        self.ch4_right = value & 0x08 != 0;
-        self.ch1_left = value & 0x10 != 0;
-        self.ch2_left = value & 0x20 != 0;
-        self.ch3_left = value & 0x40 != 0;
-        self.ch4_left = value & 0x80 != 0;
     }
 }
