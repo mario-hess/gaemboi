486155a9f3f7a179804cabec69915ad42a3f3e17
Mario Hess <office.hess@gmail.com>
2025-01-21 23:34:10 +0100

    :bug: Revert default joypad inputs

--- src/io/joypad.rs
+++ src/io/joypad.rs
@@ -50,8 +50,7 @@ impl Joypad {
         }
 
         // Return default value to not boot in multiplayer mode
-        //0xCF
-        0xFF
+        0xCF
     }
 
     pub fn handle_input(&mut self, event_handler: &EventHandler) {
